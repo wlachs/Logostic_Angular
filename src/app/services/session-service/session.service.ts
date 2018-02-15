078cd755990a0f106631a27adc3e53ed81d39481
Laszlo Benedek Borbely <laszlobenedek.borbely@ge.com>
2018-02-15 17:46:20 +0100

    exported image temporarily stored in sessionstorage, @angular/cli updated from beta (caused some routing problems)

--- src/app/services/session-service/session.service.ts
+++ src/app/services/session-service/session.service.ts
@@ -9,9 +9,10 @@ export class SessionService {
 
   storeExportableImage(imageInBase64: string) {
     this.exportableImageInBase64 = imageInBase64;
+    sessionStorage.setItem('result', imageInBase64);
   }
 
   getExportableImage() {
-    return this.exportableImageInBase64;
+    return this.exportableImageInBase64 || sessionStorage.getItem('result');
   }
 }
